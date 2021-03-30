04af3600cd5ba123af64486e37475b94d258799f
sossowski <kbrzoska97@gmail.com>
2021-03-30 21:39:21 +0200

    dodanie komponentu buttona

---
+++ src/components/common/AddButton.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  main: {
+    position: 'absolute',
+    width: '50px',
+    minWidth: '50px',
+    height: '50px',
+    borderRadius: '50%',
+    padding: 0,
+    fontSize: '20px',
+  },
+});
+
+const AddButton = (props) => {
+  const { top, right, onClickHandler, content } = props;
+  console.log(onClickHandler);
+  const classes = useStyles();
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      style={{ top, right }}
+      className={classes.main}
+      onClick={() => onClickHandler()}
+    >
+      {content}
+    </Button>
+  );
+};
+
+AddButton.propTypes = {
+  top: PropTypes.number,
+  right: PropTypes.number,
+  onClickHandler: PropTypes.func.isRequired,
+  content: PropTypes.any,
+};
+
+AddButton.defaultProps = {
+  top: 100,
+  right: 200,
+  content: '+',
+};
+
+export default AddButton;

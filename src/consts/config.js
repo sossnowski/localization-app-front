4b42e32fbced3a2c7ba73d10d1cd1158b27a4b45
sossowski <kbrzoska97@gmail.com>
2021-04-14 20:04:52 +0200

    dodanie endpointu dla socketa

--- src/consts/config.js
+++ src/consts/config.js
@@ -3,4 +3,5 @@ const apiPort = 3005;
 const apiPoint = `${apiUrl}:${apiPort}`;
 
 export const apiBase = apiPoint;
+export const socketUrl = '127.0.0.1:3005';
 export const postFilesBaseUrl = `${apiBase}/pictures/post/`;

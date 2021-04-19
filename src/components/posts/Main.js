563482d3140501cc2813fb897d8b4b8291cc7136
sossowski <kbrzoska97@gmail.com>
2021-04-19 17:25:32 +0200

    obsluga socketa dla dodwania postu

--- src/components/posts/Main.js
+++ src/components/posts/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
+import socketIOClient from 'socket.io-client';
 import { authGetRequest } from '../../helpers/apiRequests';
 import PostWrapper from './PostWrapper';
-import { setPosts } from '../../store/actions/post/post';
+import { addPost, setPosts } from '../../store/actions/post/post';
 import { sortByLikes } from '../../utils/main';
+import { socketUrl } from '../../consts/config';
+import { setSelectedLocalization } from '../../store/actions/localization/selectedLocalization';
+import UserSessionDataHandler from '../../auth/UserSessionDataHandler';
 
 const Posts = (props) => {
   const { localization } = props;
@@ -14,7 +18,20 @@ const Posts = (props) => {
 
   React.useEffect(() => {
     if (!localization) return;
+    console.log('jestem');
+    dispatch(setSelectedLocalization(localization.uid));
     getPosts();
+
+    const socket = socketIOClient(socketUrl, {
+      transports: ['websocket'],
+      query: {
+        auth: `Loc_${localization.uid}`,
+      },
+    });
+
+    socket.on('addPost', (data) => socketAddPostHandler(data));
+
+    return () => socket.emit('userLeave', `Loc_${localization.uid}`);
   }, [localization]);
 
   const getPosts = () => {
@@ -23,6 +40,11 @@ const Posts = (props) => {
     }).then((result) => {
       if (result.status === 200) dispatch(setPosts(result.data));
     });
+  };
+
+  const socketAddPostHandler = (post) => {
+    if (post.userUid !== UserSessionDataHandler.getUserData()?.uid)
+      dispatch(addPost(post));
   };
 
   React.useEffect(() => {
@@ -37,8 +59,4 @@ const Posts = (props) => {
   return postsToDisplay;
 };
 
-Posts.propTypes = {
-  localizations: PropTypes.array.isRequired,
-};
-
-export default Posts;
+export default React.memo(Posts);

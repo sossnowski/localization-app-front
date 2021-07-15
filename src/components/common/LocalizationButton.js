67bf817612116198210e42802111e5af62ac81a1
sossowski <kbrzoska97@gmail.com>
2021-07-15 19:49:59 +0200

    add user localization button to map

---
+++ src/components/common/LocalizationButton.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, makeStyles } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const useStyles = makeStyles({
+  main: {
+    position: 'absolute',
+    width: '50px',
+    minWidth: '50px',
+    height: '50px',
+    borderRadius: '50%',
+    padding: 0,
+    fontSize: '20px',
+    zIndex: 1001,
+  },
+});
+
+const LocalizationButton = (props) => {
+  const { bottom, right, setUserPosition } = props;
+  const classes = useStyles();
+
+  const localizationHandler = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setUserPosition([position.coords.longitude, position.coords.latitude]);
+    });
+  };
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      style={{ bottom, right }}
+      className={classes.main}
+      onClick={() => localizationHandler()}
+    >
+      <LocationOnIcon />
+    </Button>
+  );
+};
+
+LocalizationButton.propTypes = {
+  bottom: PropTypes.number,
+  right: PropTypes.number,
+  setUserPosition: PropTypes.func.isRequired,
+};
+
+LocalizationButton.defaultProps = {
+  right: 200,
+};
+
+export default LocalizationButton;

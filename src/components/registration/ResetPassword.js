05a9acf64e9dd2ddced7f15ae3a566efb753884d
sossowski <kbrzoska97@gmail.com>
2021-08-26 19:29:19 +0200

    clean

--- src/components/registration/ResetPassword.js
+++ src/components/registration/ResetPassword.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -16,11 +14,9 @@ import { useParams } from 'react-router-dom';
 import {
   authGetRequestWithParams,
   authPatchRequest,
-  postRequest,
 } from '../../helpers/apiRequests';
 import Languages from '../../consts/languages';
 import history from '../../history';
-import PopupLoader from '../common/Loader';
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
